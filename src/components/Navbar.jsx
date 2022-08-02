d5604d9be373e16fd8f4da7c2d3f7a53789c3a42
sergejkuhar <kukhar.s707@gmail.com>
2022-08-02 22:20:56 +0300

    added buttons to control bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
 
 const Navbar = () => (
-    <header className="flex justify-between bg-slate-700 p-4 mb-4">
-        <div className="text-white">Custom video player</div>
-        <div className="text-white">Github Logo</div>
+    <header className="flex justify-between items-center bg-slate-700 p-4 mb-4">
+        <div className="text-white text-2xl">Custom video player</div>
+        <div className="text-white">
+            <a
+                href="https://github.com/kukhars707/custom-video-player"
+                target="_blank"
+                rel="noreferrer"
+                className="text-2xl block"
+            >
+                <FontAwesomeIcon icon={faGithub} size="2x" />
+            </a>
+        </div>
     </header>
 );
 
--- src/components/VideoPlayer/controls.jsx
+++ src/components/VideoPlayer/controls.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+    faPlay,
+    faPause,
+    faArrowsSpin,
+    faForward,
+    faBackward,
+    faHurricane,
+    faExpand,
+    faVolumeHigh,
+} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 const Controls = ({onPlay, play}) => {
     return (
@@ -11,10 +22,37 @@ const Controls = ({onPlay, play}) => {
                 className="absolute top-[50%] left-[50%] transform translate-y-[-50%] translate-x-[-50%] w-24 h-24 rounded-full bg-black text-white flex justify-center items-center opacity-50"
                 onClick={onPlay}
             >
-                {play ? 'Pause' : 'Play'}
+                <FontAwesomeIcon icon={play ? faPause : faPlay} size="3x" />
             </button>
             <div className="absolute bottom-0 bg-black p-4 w-full text-white opacity-70">
-                Bootom controls
+                <div className="flex items-center">
+                    <div className="flex-none">
+                        <button onClick={onPlay} className="mr-2">
+                            <FontAwesomeIcon icon={play ? faPause : faPlay} />
+                        </button>
+                        <button className="mr-2">
+                            <FontAwesomeIcon icon={faBackward} />
+                        </button>
+                        <button className="mr-2">
+                            <FontAwesomeIcon icon={faForward} />
+                        </button>
+                        <button className="mr-2">
+                            <FontAwesomeIcon icon={faArrowsSpin} />
+                        </button>
+                        <button>
+                            <FontAwesomeIcon icon={faHurricane} />
+                        </button>
+                    </div>
+                    <div className="grow px-2">slider</div>
+                    <div className="flex-none">
+                        <button className="mr-2">
+                            <FontAwesomeIcon icon={faVolumeHigh} />
+                        </button>
+                        <button>
+                            <FontAwesomeIcon icon={faExpand} />
+                        </button>
+                    </div>
+                </div>
             </div>
         </>
     );
